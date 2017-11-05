14703a4a400c901e8cec1c3d40c9f9406aa960c6
Jennifer Sumargo <jennifersumargo@yahoo.com>
2017-11-05 03:39:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var newObj = Object.assign({}, object, {[key]: value});
+  return newObj;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
